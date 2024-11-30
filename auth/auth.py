05da94ba4d0835faf2af09ea771fc680f343bad3
Igor medvedev <medvede.kfx@gmail.com>
2024-11-30 13:17:08 +0400

    gitignor_time_jvt

--- auth/auth.py
+++ auth/auth.py
@@ -7,7 +7,7 @@ bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 SECRET = "SECRET"
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=36000)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
